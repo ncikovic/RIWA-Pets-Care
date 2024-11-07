ebaa0135805c420a76324fd9d5dc85619c7553f1
ncikovic <ncikovic@veleri.hr>
2024-11-07 12:54:47 +0100

    Projekt

---
+++ Fronted/quasar-project/src/pages/ONamaPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+</style>
+
